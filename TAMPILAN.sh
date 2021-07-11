af3c6e519f136bdf8744f92cf1177e4f512636ac
MrAdiaraaa-087 <87253374+MrAdiaraaa-087@users.noreply.github.com>
2021-07-11 10:54:43 +0700

    Add files via upload

---
+++ TAMPILAN.sh
@@ -0,0 +1,15 @@
+clear
+figlet MR.Adiaraa | lolcat
+
+
+
+
+
+
+
+
+
+echo "==============================" | lolcat
+echo "[+]Author : MR.Adiaraaa    [×]" | lolcat
+echo "[+]Team   : Cyber Attacker [×]" | lolcat
+echo "==============================" | lolcat
